ae0869de2f26a81b83fdc3beb5a6d9444d2d7c51
yunier.broche <yunier.broche@cencosudscotiabank.cl>
2023-11-21 10:10:43 -0300

    exercise 1

---
+++ src/exercise1/PositiveNegativeZero.java
@@ -0,0 +1,19 @@
+package exercise1;
+
+public class PositiveNegativeZero {
+    public static void main(String[] args) {
+      checkNumber(1);
+      checkNumber(-1);
+      checkNumber(0);
+    }
+
+    public static void checkNumber(int number) {
+        if (number == 0) {
+            System.out.println("zero");
+        } else if (number > 0) {
+            System.out.println("positive");
+        } else {
+            System.out.println("negative");
+        }
+    }
+}
