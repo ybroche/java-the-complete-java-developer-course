01452c0ca65175ecc7853746c2cb416e1e6880ec
yunier.broche <yunier.broche@cencosudscotiabank.cl>
2023-11-21 10:29:09 -0300

    exercice 5

---
+++ src/exercise5/LeapYear.java
@@ -0,0 +1,15 @@
+package exercise5;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(-1600)); // false
+        System.out.println(isLeapYear(1600)); // true
+        System.out.println(isLeapYear(2017)); // false
+        System.out.println(isLeapYear(2000)); // true
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) return false;
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    }
+}
