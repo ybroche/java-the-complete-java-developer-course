51277a93a46de8fec27bfa53d6d13fc02918ba48
yunier.broche <yunier.broche@cencosudscotiabank.cl>
2023-11-21 10:10:50 -0300

    exercise 2

---
+++ src/exercise2/SpeedConverter.java
@@ -0,0 +1,24 @@
+package exercise2;
+
+public class SpeedConverter {
+    public static void main(String[] args) {
+        printConversion(1.5);
+        printConversion(10.25);
+        printConversion(-5.6);
+        printConversion(25.42);
+        printConversion(75.114);
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) return -1;
+        return Math.round(kilometersPerHour / 1.609);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
+        System.out.println(kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
+    }
+}
