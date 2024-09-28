74857f16fb9a3fa52352e217e1e3f2750f23d0fa
yunier.broche <yunierbroche.globant@latam.com>
2024-09-28 01:59:48 -0300

    exercise 29

---
+++ src/exercise29/PaintJob.java
@@ -0,0 +1,29 @@
+package exercise29;
+
+public class PaintJob {
+    public static void main(String[] args) {
+        System.out.println(getBucketCount(3.4, 2.1, 1.5, 2)); // 3
+        System.out.println(getBucketCount(2.75, 3.25, 2.5, 1)); // 3
+        System.out.println(getBucketCount(3.4, 2.1, 1.5)); // 3
+        System.out.println(getBucketCount(7.25, 4.3, 2.35)); // 14
+        System.out.println(getBucketCount(3.4, 1.5)); // 3
+        System.out.println(getBucketCount(6.26, 2.2)); // 3
+        System.out.println(getBucketCount(3.26, 0.75)); // 5
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) return -1;
+        return (int) Math.ceil(width * height / areaPerBucket) - extraBuckets;
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0) return -1;
+        return (int) Math.ceil(width * height / areaPerBucket);
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket) {
+        if (area <= 0 || areaPerBucket <= 0) return -1;
+        return (int) Math.ceil(area / areaPerBucket);
+    }
+
+}
